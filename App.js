1cd4d89be6fab479d4d4797ac04ad1f733b28cdf
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2022-02-07 23:04:08 +0800

    Fixing foreground bluetooth

--- App.js
+++ App.js
@@ -8,37 +8,24 @@
 // import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {store} from './src/redux/store';
 import {Provider, useDispatch} from 'react-redux';
 import AppContainer from './src/navigator/App';
-import BLEBackgroundService from './src/services/BackgroundBleService';
-import BackgroundTaskServices from './src/services/BackgroundTaskService';
+
 import {createChannel} from './src/services/NotificationService';
 import {initializeFirebaseRemoteConfig} from './src/utils/remoteConfig';
 import {getData, storeData} from './src/utils/storage';
 import {LogBox} from 'react-native';
-import auth from '@react-native-firebase/auth';
-import {getUser} from './src/api';
 import {startServices} from './src/utils/initialService';
 
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 const App = () => {
-  const getCurrentUser = async () => {
-    try {
-      console.log(auth().currentUser.uid);
-      const user = await getUser(auth().currentUser.uid);
-      //console.log(user);
-      storeData('my_bluetooth_uuid', user.uuid);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const getFcmToken = async () => {
     const fcmToken = await messaging().getToken();
     if (fcmToken) {
@@ -61,12 +48,7 @@ const App = () => {
     }
   };
 
-  const getMyUUID = async () => {
-    return await getData('my_bluetooth_uuid');
-  };
-
   useEffect(() => {
-    //getCurrentUser();
     (async () => {
       try {
         const allowTracing = await getData('allow_tracing');
@@ -80,6 +62,14 @@ const App = () => {
     createChannel();
     initializeFirebaseRemoteConfig();
     requestUserPermission();
+    const messageListener = messaging().onMessage(async remoteMessage => {
+      PushNotification.localNotification({
+        message: remoteMessage.notification.body,
+        title: remoteMessage.notification.title,
+        channelId: 'test-channel',
+      });
+    });
+    return messageListener;
   });
   return (
     <Provider store={store}>
